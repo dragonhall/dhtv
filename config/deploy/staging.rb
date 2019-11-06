ba35747d5ff0f08a16396d72a68695fac49469fd
Gabor Garami <hron@hron.me>
2019-11-06 10:23:49 +0100

    Add support to customize deploy branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,8 @@ set :nginx_sites_enabled_path, '/opt/nginx-with-rtmp/conf/sites-enabled'
 
 set :deploy_to, '/srv/www/tv.teszt.dragonhall.hu/htdocs'
 set :nginx_server_name, 'tv.teszt.dragonhall.hu'
-set :branch, 'develop'
+set :branch, ENV.fetch('DEPLOY_BRANCH') { 'develop' }.to_sym
+
 
 set :ssh_options, keys: %W[#{ENV['HOME']}/.ssh/id_rsa], auth_methods: %w[publickey] #, verbose: :debug
 
